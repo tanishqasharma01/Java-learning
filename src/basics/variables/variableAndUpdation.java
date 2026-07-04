dd1bce88a19de741179fdd65e584288b839c45ee
tanishqa <tanishqasharma09@gmail.com>
2026-07-04 14:46:51 +0530

    Complete variables and updation topic

---
+++ src/basics/variables/variableAndUpdation.java
@@ -0,0 +1,9 @@
+package Basics.variables;
+
+public class variableAndUpdation {
+    public static void main(String[] args) {
+        int x ; // declaration
+        x = 6; //initialization
+        System.out.println(x);
+    }
+}
