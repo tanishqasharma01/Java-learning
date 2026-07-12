930260a1640a1cdf8a8da3f76afb1e20b0b25e9c
tanishqa <tanishqasharma09@gmail.com>
2026-07-12 13:40:10 +0530

    Naming varibale rule

---
+++ src/basics/variables/NamingVariable.java
@@ -0,0 +1,16 @@
+/*VARIABLE NAMING RULE:
+1) varibale can start from an alphabet or underscore _ or $.
+2) special characters except _ and $ are not allowed.
+3) some particular keywords are not allowed.
+4) Commas or blanks are not allowed
+ */
+
+package basics.variables;
+
+
+public class NamingVariable {
+    public static void main(String[] args) {
+
+    }
+}
+
