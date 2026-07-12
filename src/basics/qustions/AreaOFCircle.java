245a034b624abd49b8acf6e3ab26d25268efcc54
tanishqa <tanishqasharma09@gmail.com>
2026-07-12 13:31:04 +0530

    done some very basic qustions

---
+++ src/basics/qustions/AreaOFCircle.java
@@ -0,0 +1,10 @@
+package basics.qustions;
+
+public class AreaOFCircle {
+    public static void main(String[] args) {
+        double r = 5.2;
+        double area = 3.1415*r*r;
+
+        System.out.println("Area of circle is : " + area);
+    }
+}
---
+++ src/basics/qustions/VolumeOfCircle.java
@@ -0,0 +1,9 @@
+package basics.qustions;
+
+public class VolumeOfCircle {
+    public static void main(String[] args) {
+        double r = 4.8;
+        double vol = 4*3.14*r*r*r/3;
+        System.out.println(vol);
+    }
+}
