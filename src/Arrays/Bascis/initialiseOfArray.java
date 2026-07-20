092568a71efc6ba5b8843e24f4430f271f0b6b33
tanishqa <tanishqasharma09@gmail.com>
2026-07-20 15:59:27 +0530

    initialize of array

---
+++ src/Arrays/Bascis/initialiseOfArray.java
@@ -0,0 +1,12 @@
+package Arrays.Bascis;
+
+public class initialiseOfArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,1,2,3,4,5,6,7,8,9,11};
+        int n = arr.length;
+        System.out.println(arr.length);
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
