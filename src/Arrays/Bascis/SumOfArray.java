e39b1fb83cb09735d96fc9cb6a9891888c7bbb4c
tanishqa <tanishqasharma09@gmail.com>
2026-07-20 16:13:42 +0530

    Sum of array

--- src/Arrays/Bascis/SumOfArray.java
+++ src/Arrays/Bascis/SumOfArray.java
@@ -1,4 +1,13 @@
 package Arrays.Bascis;
 
 public class SumOfArray {
+    public static void main(String[] args) {
+        int sum=0;
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
 }
