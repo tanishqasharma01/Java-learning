16e83938e2286d3ae3792074f4010bb3c9783c0d
tanishqa <tanishqasharma09@gmail.com>
2026-07-20 15:30:29 +0530

    Syntax of array

---
+++ src/Arrays/Bascis/SyntaxOfArrays.java
@@ -0,0 +1,18 @@
+//An array is a data structure that stores multiple elements of the same data type in contiguous (continuous) memory locations.
+package Arrays.Bascis;
+
+
+public class SyntaxOfArrays {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        // initialising individual elements
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        System.out.println(arr[0]);
+
+    }
+}
---
+++ src/basics/Operators/Hierarchy.java
@@ -0,0 +1,4 @@
+package basics.Operators;
+
+public class Hierarchy {
+}
